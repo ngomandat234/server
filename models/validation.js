829efd44dbd17c38524133a93ea5b340b92969e9
20522094_letruyen <20522094@gm.uit.edu.vn>
2022-05-04 17:07:46 +0700

    Update from master

--- models/validation.js
+++ models/validation.js
@@ -1,8 +1,9 @@
 const Joi = require('@hapi/joi');
+
+// Register Validate
 const registerValidation = function(data){
     const schema = Joi.object ({
-        name: Joi.string()
-                 .min(4),
+        name:Joi.string(),
         email: Joi.string()
                    .email()
                    .min(6)
@@ -10,12 +11,23 @@ const registerValidation = function(data){
         password: Joi.string()
                    .min(6)
                    .required(),
-        phone: Joi.string()
-                   .min(6),
-        role: Joi.string()
-                   .min(2)
+        phone : Joi.string(),
+        role : Joi.string()
                           
     })
    return  schema.validate(data)
 }
-module.exports.registerValidation = registerValidation
+const loginValidation = function(data){
+    const schema = Joi.object ({
+        email: Joi.string()
+                   .email()
+                   .min(6)
+                   .required(),
+        password: Joi.string()
+                   .min(6)
+                   .required(),
+                          
+    })
+   return  schema.validate(data)
+}
+module.exports = {registerValidation,loginValidation}
--- routers/auth.js
+++ routers/auth.js
@@ -3,15 +3,9 @@ const router= express.Router()
 const checkEmail = require("../middleware/verifySignUp")
 const checkAdmin = require("../middleware/checkAdmin")
 const auth = require("../middleware/authentication")
-const {registerValidation} = require("../models/validation")
 const authUserController = require('../controllers/authUserController')
-router.post('/register',checkEmail,authUserController.register,function(req,res)
-{
-    const{ error } = registerValidation(req.body);
-    if(error) 
-    return res.status(400).send(error.details[0].message)
-}
-)
+//const { check, validationResult } = require('express-validator')
+router.post('/register',authUserController.register)
 router.post('/login',authUserController.login)
 router.post('/refreshToken',checkEmail, authUserController.refreshToken)
 module.exports=router
